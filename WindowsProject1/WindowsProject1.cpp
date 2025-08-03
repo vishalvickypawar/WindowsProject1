9e0b0efb0f24f5d98449071876ab31934c6e8c32
Vishal Pawar <168279073+vishalvickypawar@users.noreply.github.com>
2025-08-03 22:50:33 +0530

    Drawing

--- WindowsProject1/WindowsProject1.cpp
+++ WindowsProject1/WindowsProject1.cpp
@@ -2,6 +2,7 @@
 
 LRESULT CALLBACK myfunc(HWND, UINT, WPARAM, LPARAM);
 WNDCLASS a;
+int flag = 0,x1,y1,x2,y2;
 
 int APIENTRY WinMain(HINSTANCE i, HINSTANCE j, char *k, int l)
 {
@@ -29,15 +30,31 @@ int APIENTRY WinMain(HINSTANCE i, HINSTANCE j, char *k, int l)
 LRESULT CALLBACK myfunc(HWND w, UINT x, WPARAM y, LPARAM z)
 {
     HDC d;
-	PAINTSTRUCT p;
-	RECT r;
     switch (x)
     {
-        case WM_PAINT:
-            d = BeginPaint(w, &p);
-            GetClientRect(w, &r);
-			DrawText(d, L"Hello!", -1, &r, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
-            EndPaint(w, &p);
+        case WM_LBUTTONDOWN:
+            if (!flag)
+            {
+				x1 = LOWORD(z);
+                y1 = HIWORD(z);
+                flag = 1;
+            }
+            break;
+        case WM_MOUSEMOVE:
+            if(flag)
+            {
+				x2 = LOWORD(z);
+                y2 = HIWORD(z);
+                d = GetDC(w);
+                MoveToEx(d, x1, y1, 0);
+                LineTo(d, x2, y2);
+                ReleaseDC(w, d);
+                x1 = x2;
+				y1 = y2;
+			}
+			break;
+        case WM_LBUTTONUP:
+            flag = 0;
 			break;
         case WM_DESTROY:
             PostQuitMessage(0);
